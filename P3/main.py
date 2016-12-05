5d9eef30b244fc22e0c620575b3dd17a7cb86247
mgmacias95 <mgmacias95@correo.ugr.es>
2016-12-05 18:26:23 +0100

    ya obtengo las coordenadas de píxel :)

--- P3/main.py
+++ P3/main.py
@@ -13,6 +13,7 @@ from funciones import *
 
 if __name__ == '__main__':
     P = genera_camara_finita()
-    c1, c2 = genera_puntos_planos_ortogonales_distintos()
-    c = proyecta_puntos_en_plano(camara=P, puntos1=c1, puntos2=c2)
+    p = genera_puntos_planos_ortogonales_distintos()
+    c = proyecta_puntos_en_plano(camara=P, puntos=p)
     print(c)
+
